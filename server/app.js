bc850e63c7c9acf7103d63c6332d82e435286fe2
HoonWi <wee0702@gmail.com>
2023-02-13 15:25:29 +0900

    앱 빌드 설정

--- server/app.js
+++ server/app.js
@@ -5,8 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.static('build'));
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/build/index.html');
 });
 app.listen(3000, () => {
     console.log('server is running!');
